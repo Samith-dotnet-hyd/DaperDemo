060f59854ddaefb92d6f05095d487ae7fe1f879b
Kanuganti Suraj <Kanuganti.Suraj@encora.com>
2025-10-29 18:14:07 +0530

    This changes made by Kanuganti suraj

---
+++ DaperDemo/Models/Suraj_GitCollab.cs
@@ -0,0 +1,9 @@
+﻿namespace DaperDemo.Models
+{
+    public class Suraj_GitCollab
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
